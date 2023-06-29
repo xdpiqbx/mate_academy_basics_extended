a17f11b2ac589e07f1731a8292f9559131561fe4
xdpiqbx <xdpiqbx@gmail.com>
2023-06-29 23:48:05 +0300

    in process

--- 002_basics_extended/Daily_tasks/001_get_order.py
+++ 002_basics_extended/Daily_tasks/001_get_order.py
@@ -157,6 +157,152 @@
 # jewels_and_stones("z", "ZZ")
 # jewels_and_stones("abcde", "abcd")
 
+# ============================================================================ List Comparator
+# def list_comparator(first: list, second: list) -> int:
+#     count = 0
+#     for item in first:
+#         if item in second:
+#             count += 1
+#     return count
+
+# ============================================================================ Is Leap year
+# def is_leap_year(year: int) -> bool:
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return True
+#     else:
+#         return False
+
+# ============================================================================ Get Section id
+# def get_section_id(scroll: int, sizes: list) -> int:
+#     index = 0
+#     for size in sizes:
+#         scroll -= size
+#         if scroll < 0:
+#             return index
+#         else:
+#             index += 1
+#     return -1
+
+# print(get_section_id(300, [300, 200, 400]))
+# print(get_section_id(1600, [300, 200, 400, 600, 100]))
+# print(get_section_id(500, [300, 200, 400, 600, 100]))
+
+# # ============================================================================ Convert to title
+# def convert_to_title(num: int) -> str:
+#     div = num // 26
+#     mod = num % 26
+#     # print(div)  # first letter
+#     # print(mod)  # second letter
+#     #
+#     # # print(f"{chr(65)} = {ord('A')}")
+#     # # print(f"{chr(90)} = {ord('Z')}")
+#     #
+#     # print(f"{chr(div + 64)} = {ord(chr(div + 64))}")
+#     # print(f"{chr(mod + 64)} = {ord(chr(mod + 64))}")
+#     div = num // 26
+#     mod = num % 26
+#     if div > 0 and mod > 0:
+#         return f"{chr(div + 64)}{chr(mod + 64)}"
+#     if div == 0 and mod > 0:
+#         return chr(mod + 64)
+#     if div > 0 and mod == 0:
+#         return f"{chr(div - 1 + 64)}{chr(mod + 64 + 26)}" if div != 1 else chr(mod + 64 + 26)
+#
+#     # return chr(mod + 64) if div == 0 else f"{chr(div + 64)}{chr(mod + 64)}"
+#
+# print(convert_to_title(26))  # Z
+# print("========================")
+# print(convert_to_title(52))   # AZ
+# print("========================")
+# print(convert_to_title(78))   # BZ
+# print("========================")
+# print(convert_to_title(104))   # CZ
+# print("========================")
+# print(convert_to_title(28))  # AB
+# print("========================")
+# print(convert_to_title(701))  # ZY
+# print("========================")
+# print(convert_to_title(1))  # A
+# print("========================")
+
+# # ============================================================================ Rotate List IN PROCESS!!!!!
+# from itertools import cycle
+# def rotate_list(nums: list, steps: int) -> list:
+#     len_nums = len(nums) + 1
+#     count = 0
+#     res = []
+#     for num in cycle(nums):
+#         if len_nums + steps == count:
+#             break
+#         res.append(num)
+#         count += 1
+#     return res[steps+1:]
+#
+#
+# print(rotate_list([1, 2, 3, 4, 5, 6, 7], 3))
+# print(rotate_list([-1, -100, 3, 99], 2))
+
+# # ============================================================================ Count 9
+# def count_nines(number: int) -> int:
+#     s = ""
+#     for i in range(1, number + 1):
+#         s += str(i)
+#     return s.count("9")
+# print(count_nines(1000))
+
+# # ============================================================================ Middle Char
+# def middle_character(word: str) -> str:
+#     len_word = len(word)
+#     if len_word == 1:
+#         return word
+#     if len_word == 0:
+#         return ""
+#     middle = len_word // 2
+#     return word[middle - 1] + word[middle] if len_word % 2 == 0 else word[middle]
+
+# # ============================================================================ Multiple 3 5
+# def multiples_3_5(number: int) -> int:
+#     s = 0
+#     for i in range(number):
+#         if i % 15 == 0:
+#             s += i
+#             continue
+#         elif i % 3 == 0 or i % 5 == 0:
+#             s += i
+#     return s
+# print(multiples_3_5(10))
+
+# # ============================================================================ Credit card issue checking
+# def credit_card_issuer_checking(number: int) -> str:
+#     str_num = str(number)
+#     size = len(str_num)
+#     if size < 13:
+#         return "Unknown"
+#
+#     short_num = int(str(number)[:4])
+#     first_num = int(short_num // 1000)
+#
+#     if first_num not in range(3, 6 + 1):
+#         return "Unknown"
+#
+#     if (short_num // 100 == 34 or short_num // 100 == 37) and size == 15:
+#         return "AMEX"
+#     elif short_num == 6011 and size == 16:
+#         return "Discover"
+#     elif short_num // 100 in range(51, 55 + 1):
+#         return "Mastercard"
+#     elif first_num == 4 and (size == 13 or size == 16):
+#         return "VISA"
+#
+#     return "Unknown"
+
+
 
 
 
